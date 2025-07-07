b83bf72f4844edca5cc38184a583520e40106648
revanth102938 <revanth.ace66@gmail.com>
2025-07-07 23:15:36 +0530

    minor change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000", 
+        target: "https://travel-mate-41yk.onrender.com", 
         changeOrigin: true,
       },
     },
